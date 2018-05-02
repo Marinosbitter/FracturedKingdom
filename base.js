408cebb43667f7625230d403b1fec6e6e052682c
Marijn Bitter <marijn_bitter@goeiezaak.com>
2018-05-02 12:51:20 +0200

    Made noise more consistent

--- base.js
+++ base.js
@@ -1,14 +1,20 @@
 var canvas = document.getElementById('map');
 var ctx = canvas.getContext("2d");
 
-noise.seed(42);
+var zoom = 7;
+var levels = 255/5;
+
+noise.seed(Math.random());
 for (var x = 0; x < canvas.width; x++) {
     for (var y = 0; y < canvas.height; y++) {
         // All noise functions return values in the range of -1 to 1.
 
-        var elevation = noise.simplex2(x, y);
-        elevation *= 255;
-        elevation = Math.abs(elevation);
+        var nx = x/canvas.width - 0.5, ny = y/canvas.height - 0.5;
+        var elevation = noise.simplex2(nx * zoom, ny * zoom);
+
+        elevation = 255 * elevation;
+        elevation = (elevation + 255) / 2;
+        elevation = Math.ceil(elevation / levels) * levels;
         var r = elevation;
         var g = elevation;
         var b = elevation;
@@ -16,5 +22,9 @@ for (var x = 0; x < canvas.width; x++) {
 
         ctx.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
         ctx.fillRect( x, y, 1, 1 );
+        
+        if(y==0){
+            console.info(elevation);
+        }
     }
 }
