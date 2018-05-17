b3fff2456d4d13d6b48ce89f8fb766f68459c0b7
Marijn Bitter <marijn_bitter@live.nl>
2018-05-18 00:26:12 +0200

    Testing tiling the coords

--- base.js
+++ base.js
@@ -1,93 +1,139 @@
-var map;
+/**
+ * Initialiezes the Google map
+ * @returns {string} Google map object
+ */
 function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 0, lng: 0},
+        zoom: 1,
+        streetViewControl: false,
+        mapTypeControlOptions: {
+            mapTypeIds: ['fractured']
+        }
+    });
+
+    // Displayes 
+    CoordMapType.prototype.getTile = function(coord, zoom, ownerDocument) {
+        var normalizedCoord = getNormalizedCoord(coord, zoom);
+        if (!normalizedCoord) {
+            return null;
+        }
+        var canvas = ownerDocument.createElement('canvas');
+        canvas.width = this.tileSize.width;
+        canvas.height = this.tileSize.height;
+        canvas.style.width = this.tileSize.width + 'px';
+        canvas.style.height = this.tileSize.height + 'px';
+        canvas.style.borderStyle = 'solid';
+        canvas.style.borderWidth = '1px';
+        canvas.style.borderColor = '#AAAAAA';
+        canvas = generateCanvas(canvas, normalizedCoord, zoom);
+        return canvas;
+    };
+
+    // This is only needed for reference and should not be visible
+    var fracturedMapType = new google.maps.ImageMapType({
+        getTileUrl: function(coord, zoom) {
+            var normalizedCoord = getNormalizedCoord(coord, zoom);
+            if (!normalizedCoord) {
+                return null;
+            }
+            var bound = Math.pow(2, zoom);
+            return '//via.placeholder.com/256x256?text=Z:+' + 
+                zoom + '+X:+' + normalizedCoord.x + '+Y:+' +
+                (bound - normalizedCoord.y - 1);
+        },
+        tileSize: new google.maps.Size(256, 256),
+        maxZoom: 9,
+        minZoom: 0,
+        name: 'Fractured Kingdom'
+    });
+
+    map.mapTypes.set('fractured', fracturedMapType);
+    map.setMapTypeId('fractured');
+    map.overlayMapTypes.insertAt(
+        0, new CoordMapType(new google.maps.Size(256, 256)));
+
+    // Helper functions
     function CoordMapType(tileSize) {
         this.tileSize = tileSize;
     }
+}
+function getTileSize(){
+    var tileSize = {
+        x: 256,
+        y: 256
+    }
+    return tileSize;
+}
+/**
+ * Normalizes the coords that tiles repeat across the x axis (horizontally) like the standard Google map tiles.
+ * @param   {object} The  coordinate of the tile
+ * @param   {number} zoom Current zoom level of the map
+ * @returns {object} Normalized Coord
+ */
+function getNormalizedCoord(coord, zoom) {
+    var y = coord.y;
+    var x = coord.x;
 
-    CoordMapType.prototype.getTile = function(coord, zoom, ownerDocument) {
-        console.info("Map zoom: " + map.getZoom());
-        
-        var canvas = ownerDocument.createElement('canvas');
-        canvas.style.width = this.tileSize.width + 'px';
-        canvas.style.height = this.tileSize.height + 'px';
-        var ctx = canvas.getContext("2d");
+    // tile range in one direction range is dependent on zoom level
+    // 0 = 1 tile, 1 = 2 tiles, 2 = 4 tiles, 3 = 8 tiles, etc
+    var tileRange = 1 << zoom;
 
-        //** Inputs **//
-        var inputSeed = 42  ;
-        var inputZoom = 1; // AKA Zoom
-        var inputExp = 2; // AKA Normalization
-        var inputWaterLevel = 0.4; // AKA amm of water
-        var inputTerraces = 20; // AKA Heightlines (aestetic)
-
-        //** Normalize inputs **//
-        var seed = inputSeed;
-        var freq = map.getZoom() + 1;
-        var exp = inputExp;
-        var waterLevel = inputWaterLevel;
-        var terraces = inputTerraces;
-        
-        console.info(freq);
-
-        noise.seed(inputSeed);
-        for (var x = 0; x < canvas.width; x++) {
-            for (var y = 0; y < canvas.height; y++) {
-                var r = 0;
-                var g = 0;
-                var b = 0;
-                var a = 0;
-                
-                var mapsX = x;
-                var mapsY = y;
-
-                var nx = mapsX/canvas.width - 0.5, ny = mapsY/canvas.height - 0.5;
-
-                //== Landscape ==//
-                var elevation = noise.simplex2(freq * nx, freq * ny);
-                elevation += 0.5 * noise.simplex2(freq * 2 * nx, freq * 2 * ny);
-                elevation += 0.25 * noise.simplex2(freq * 4 * nx, freq * 2 * ny);
-
-                elevation = Math.abs(elevation);    // Make number positive
-                elevation = Math.pow(elevation, inputExp);
-
-                r = elevation;
-                g = elevation;
-                b = elevation;
-
-                //== Water Level ==//
-                if(elevation < waterLevel){
-                    b = elevation / waterLevel;
-                    r = 0;
-                    g = 0;
-                }
-
-                //** Enabeling terraces **//
-                r = Math.round(r * terraces) / terraces;
-                g = Math.round(g * terraces) / terraces;
-                b = Math.round(b * terraces) / terraces;
-
-                //** Shifting values to 0-255 **//
-                r = r * 200;
-                g = g * 200;
-                b = b * 200;
-                a = 255;
-
-                //** Setting colors and painting on canavs **//
-                ctx.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
-                ctx.fillRect( x, y, 1, 1 );
-            }
-        }
-
-        return canvas;
+    // don't repeat across y-axis (vertically)
+    if (y < 0 || y >= tileRange) {
+        return null;
     }
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 52.0769272, 
-            lng: 4.4862979
-        },
-        zoom: 0
-    });
+    // repeat across x-axis
+    if (x < 0 || x >= tileRange) {
+        x = (x % tileRange + tileRange) % tileRange;
+    }
 
-    map.overlayMapTypes.insertAt(
-        0, new CoordMapType(new google.maps.Size(256, 256)));
+    return {x: x, y: y};
+}
+
+/**
+ * Fills a canvas for a Google Maps tile
+ * @param   {object} canvas The canvas used on the tile
+ * @param   {object} coord  The tile coordinate
+ * @param   {number} zoom   Zoom level of the ma[]
+ * @returns {canvas} The painted canvas
+ */
+function generateCanvas(canvas, coord, zoom){
+    var ctx = canvas.getContext("2d");
+
+    var levels = 20;
+    levels = 12;
+
+    var waterLevel = 0.3;
+    
+    noise.seed(43);
+    for (var x = 0; x < canvas.width; x++) {
+        for (var y = 0; y < canvas.height; y++) {
+            // Adjusting x and y coordinates to tiled versions
+            var cX = x + (getTileSize.x * (coord.x + 1));
+            var cY = y + (getTileSize.y * (coord.y + 1));
+            var h, s, l;
+            // All noise functions return values in the range of -1 to 1.
+            var elevation = noise.simplex2(0.004 * cX, 0.004 * cY);
+            elevation += 0.2 * noise.simplex2(0.02 * cX, 0.02 * cY);
+            elevation += 0.05 * noise.simplex2(0.04 * cX, 0.04 * cY);
+
+            elevation = (elevation +1) / 2;
+
+            if(elevation < waterLevel){
+                h = 130;
+                l = 34;
+            } // Is land
+            else {
+                h = 184;
+                l = 50;
+            } // Is water
+            s = 100;
+
+            ctx.fillStyle = "hsl("+h+", "+s+"%, "+l+"%)";
+            ctx.fillRect( x, y, 1, 1 );
+        }
+    }
+    return canvas;
 }
