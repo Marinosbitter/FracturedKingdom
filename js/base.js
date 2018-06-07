2b806f03966d6c5325c0a18424ecc928639de429
Marijn Bitter <marijn_bitter@goeiezaak.com>
2018-06-07 10:15:27 +0200

    Added a normalizer

--- js/base.js
+++ js/base.js
@@ -23,22 +23,28 @@ var mapVariables = {
 
 
 var canvas = document.getElementById("map");
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
 var ctx = canvas.getContext("2d");
 
-noise.seed(Math.random());
+noise.seed(42);
 
+var count = Object.keys(mapVariables.octaves).length;
+var normalizer = 0;
+for (i = 0; i < count; i++){
+    normalizer += mapVariables.octaves[i].weight;
+}
 for (var x = 0; x < canvas.width; x++) {
     for (var y = 0; y < canvas.height; y++) {
         var value = 0;
-        var count = Object.keys(mapVariables.octaves).length;
         for(i = 0; i < count; i++){
-            value += mapVariables.octaves[i].weight * noise.simplex2(mapVariables.octaves[i].freqModX * mapVariables.frequency * x, mapVariables.octaves[i].freqModY * mapVariables.frequency * y);
+            value += mapVariables.octaves[i].weight * noise.simplex2(mapVariables.octaves[i].freqModX * mapVariables.frequency * x, mapVariables.octaves[i].freqModY * mapVariables.frequency * y - 100);
         }
-        value = (value+1)/2;
+        value = (value+normalizer)/(2*normalizer); // Brings the values from -1 to 1 back to 0 to 1
         value = Math.pow(value, mapVariables.redistribution);
-        
-        ctx.fillStyle = "rgba(0, 0, 0, "+value+")";
 
+        ctx.fillStyle = "rgba(0, 0, 0, "+value+")";
+        // For debugging, makes values below 0 red, above 1 green
         if(value <= 0){
             ctx.fillStyle = "#ff0000";
         }
@@ -47,5 +53,4 @@ for (var x = 0; x < canvas.width; x++) {
         }
         ctx.fillRect(x,y,1,1);
     }
-    console.info(value);
 }
